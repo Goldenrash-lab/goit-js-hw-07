1ee5c3724e1fb310586c2eaa7f8e31cd058350b3
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-11-01 17:23:30 +0200

    task1 with add task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,6 +2,7 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const listEl = document.querySelector(".gallery");
+let instance = null;
 
 function imageItems(items) {
   return items.map(
@@ -23,23 +24,39 @@ function renderItems(items) {
   const markup = imageItems(items).join("");
   listEl.insertAdjacentHTML("afterbegin", markup);
 }
+
 function onImgClick(e) {
   e.preventDefault();
   if (e.target.nodeName !== "IMG") return;
   else {
     const urlPic = e.target.dataset.source;
-    basicLightbox
-      .create(
-        `
+
+    instance = basicLightbox.create(
+      `
 		<img width="1400" height="900" src="${urlPic}">
-	`
-      )
-      .show();
+	`,
+      {
+        onShow: (instance) => {
+          console.log("show");
+          document.addEventListener("keydown", onInstanceKeydown);
+        },
+        onClose: (instance) => {
+          console.log("close");
+          document.removeEventListener("keydown", onInstanceKeydown);
+        },
+      }
+    );
     // console.log(urlPic);
+    instance.show();
+  }
+}
+
+function onInstanceKeydown(e) {
+  if (e.code !== "Escape") return;
+  else {
+    instance.close();
   }
 }
 
 renderItems(galleryItems);
 listEl.addEventListener("click", onImgClick);
-
-// console.log(galleryItems);
